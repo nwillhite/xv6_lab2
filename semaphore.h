49106d76b84990ec9dba71b49b57fd7e2075a7a5
Nicholas Willhite <nwill016@sledge.cs.ucr.edu>
2016-05-27 10:52:14 -0700

    implementing semaphores

---
+++ semaphore.h
@@ -0,0 +1,15 @@
+lude "param.h"
+#include "queue.h"
+#include "spinlock.h"
+
+typedef struct Semaphore {
+        int value;
+            struct queue *q;
+                struct spinlock lock;
+
+}Semaphore;
+
+//functions
+void sem_init(struct Semaphore *s, int val);
+void sem_acquire(Semaphore *s);
+void sem_signal(Semaphore *s);
